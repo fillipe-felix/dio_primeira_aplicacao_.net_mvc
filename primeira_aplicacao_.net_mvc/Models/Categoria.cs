8fd244c7529e08410fae4a865829af2a82066e44
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-17 20:59:57 -0300

    - Colocando dataAnnotation para que uma variavel apareça com outro nome - Colocando a anotação Required para fazer validação no front de alguns campos -Colocando anotation Range

--- primeira_aplicacao_.net_mvc/Models/Categoria.cs
+++ primeira_aplicacao_.net_mvc/Models/Categoria.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace primeira_aplicacao_.net_mvc.Models
 {
     public class Categoria
     {
         public int id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
         public string Descricao { get; set; }
         
         public List<Produto> Produtos { get; set; }
