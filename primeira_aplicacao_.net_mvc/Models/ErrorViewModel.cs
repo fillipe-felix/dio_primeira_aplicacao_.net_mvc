7436ea535ebeb1504bc2b7d844b518c6d4e0dec3
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-16 21:51:35 -0300

    first commit

---
+++ primeira_aplicacao_.net_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace primeira_aplicacao_.net_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
